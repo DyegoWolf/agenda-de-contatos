040e7d5022cfe74f14174dad58c331bf93cfa729
Dyego Magno <dyegowolf@gmail.com>
2019-10-07 15:40:43 -0300

    Banco de dados inicial com scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :phones
+  resources :addresses
+  resources :contacts
+  resources :kinds
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "static_pages#index"
 
